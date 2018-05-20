0862ede77a5717eb0efdb9407c72e5caac5630de
l08084 <l08084.1989@gmail.com>
2018-05-20 20:15:33 +0900

    app.module.tsにtouch ID プラグインを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { TouchID } from '@ionic-native/touch-id';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    TouchID,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
