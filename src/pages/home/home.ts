f9f22c3064aa2741fbfe44cbed7bb288dad70b0f
l08084 <l08084.1989@gmail.com>
2018-06-03 15:48:25 +0900

    暗号化・復号化

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { TouchID } from "@ionic-native/touch-id";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  template: `<ion-content padding>
+              <button (click)="login()" ion-button>Login</button>
+            </ion-content>`
 })
 export class HomePage {
+  constructor(private touchId: TouchID) {}
 
-  constructor(public navCtrl: NavController) {
+  login() {
+    // Touch ID, Face ID 対応端末かどうか確認する
+    this.touchId
+      .isAvailable()
+      .then(
+        res => console.log("TouchID is available!"),
+        err => console.error("TouchID is not available", err)
+      );
 
+    // 確認ダイアログを出したあと、Touch ID, Face ID 認証を実行する
+    this.touchId
+      .verifyFingerprint("Scan your fingerprint please")
+      .then(res => console.log("Ok", res), err => console.error("Error", err));
   }
-
 }
